0afc18298edd17b8cc6a101eb99bd22fe09ab3d2
D.A Jalloh <info@codewithjalloh.com>
2016-08-04 22:38:14 +0100

    comment added to code

--- showIP/ViewController.swift
+++ showIP/ViewController.swift
@@ -24,17 +24,21 @@ class ViewController: UIViewController {
         
         var addresses = [String]()
         
+        // get list of all interfaces on the local machine
         var ifaddr: UnsafeMutablePointer<ifaddrs> = nil
         if getifaddrs(&ifaddr) == 0 {
             
+            // for each interface
             for (var ptr = ifaddr; ptr != nil; ptr = ptr.memory.ifa_next) {
                 let flags = Int32(ptr.memory.ifa_flags)
                 var addr = ptr.memory.ifa_addr.memory
                 
+                
+                // check for running IPv4, IPv6 interfaces. skip the loopback interface
                 if (flags & (IFF_UP|IFF_RUNNING|IFF_LOOPBACK)) == (IFF_UP|IFF_RUNNING) {
                     if addr.sa_family == UInt8(AF_INET) || addr.sa_family == UInt8(AF_INET6) {
                         
-                        
+                        // convert interface address to a human readable string
                         var hostname = [CChar](count: Int(NI_MAXHOST), repeatedValue: 0)
                         if (getnameinfo(&addr, socklen_t(addr.sa_len), &hostname, socklen_t(hostname.count),nil, socklen_t(0), NI_NUMERICHOST) == 0) {
                             if let address = String.fromCString(hostname) {
